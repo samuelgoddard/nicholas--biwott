f754d789b90f41cba628a499f2ebe46d6f09d5a2
Sam Goddard <hello@samgoddard.co.uk>
2022-10-19 12:06:56 +0100

    live domain added

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,4 +2,5 @@
 module.exports = {
   siteUrl: 'https://nicholasbiwott.com',
   generateRobotsTxt: true,
+  exclude: ['/quotes', '/sources', '/terms-and-conditions', '/press-template', '/archive/*',],
 }
